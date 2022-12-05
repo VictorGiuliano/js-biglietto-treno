d828497561c77fffaa57d9679f4b43d805985a09
VictorGiuliano <victorgiuliano98@gmail.com>
2022-12-05 21:36:34 +0100

    Script sconto

--- script.js
+++ script.js
@@ -9,16 +9,40 @@ per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.*/
 
 /*
 1.Chiedo all'utente quanti km deve percorrere
-2.Chiedo all'utente età del passeggero
-3.Calcolare prezzo del biglietto
+2.Chiedo all'utente età del passeggero 
+3.Calcolare prezzo del biglietto base
+4.Calcolo dello prezzo finale
 */
 
 //1.
-const kmUser = prompt('Quanti km devi percorrere?').trim();
+const kmUser = parseInt(prompt('Quanti km devi percorrere?').trim());
 //console.log(kmUser);
 
 //2.
-const ageUser = prompt('Quanti hanni hai?', 23).trim();
+const ageUser = parseInt(prompt('Quanti hanni hai?', 23).trim());
 //console.log(ageUser);
+const tariffa = 0.21;
 
 //3.
+const costoBase = kmUser * tariffa;
+//console.log(costoBase);
+
+//4.
+let costoScontato;
+let sconto = 25 / 100;
+
+if(isNaN(kmUser) || isNaN(ageUser)){
+    alert ("Devi inserire un numero");
+} else {
+    if(ageUser <= 18){
+        costoScontato = costoBase - sconto;
+    }else if(ageUser > 65){
+        sconto = 40 / 100;
+        costoScontato = costoBase - sconto;
+    }   
+    console.log(costoScontato);
+}
+
+
+
+
